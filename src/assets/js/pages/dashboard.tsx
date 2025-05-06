f360e5674d6d20e09865017175a44cb3b3172518
markusvannorlen <markusweblance@gmail.com>
2025-05-06 17:44:53 +0300

    added grid layout

--- src/assets/js/pages/dashboard.tsx
+++ src/assets/js/pages/dashboard.tsx
@@ -1,86 +1,53 @@
 import AppLayout from '@/layouts/app-layout';
 import {type BreadcrumbItem} from '@/types';
-import {Button} from '@/components/ui/button';
-import {
-    DialogStack,
-    DialogStackBody,
-    DialogStackContent,
-    DialogStackDescription,
-    DialogStackNext,
-    DialogStackOverlay,
-    DialogStackPrevious,
-    DialogStackTitle,
-    DialogStackTrigger,
-} from '@/components/ui/dialog-stack.tsx';
+import {Responsive, WidthProvider} from "react-grid-layout";
+
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+import {useMemo, useState} from "react";
 
 const breadcrumbs: BreadcrumbItem[] = [];
 
+const initialLayout = [
+    {x: 0, y: 0, w: 1, h: 1, i: "1111"},
+    {x: 1, y: 0, w: 2, h: 1, i: "2222"},
+    {x: 4, y: 0, w: 1, h: 2, i: "3333"},
+];
+
 export default function Dashboard() {
+    const [layout, setLayout] = useState(initialLayout);
+    const ResponsiveGridLayout = useMemo(() => WidthProvider(Responsive), []);
+
+    const handleLayoutChange = (currentLayout: any) => {
+        setLayout(currentLayout);
+        console.log("Updated layout:", currentLayout);
+    };
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 Dashboard
-
-                <DialogStack>
-                    <DialogStackTrigger asChild>
-                        <Button variant="outline">Open</Button>
-                    </DialogStackTrigger>
-
-                    <DialogStackOverlay/>
-
-                    <DialogStackBody>
-                        <DialogStackContent>
-                            <div className="flex flex-col space-y-1.5 text-center sm:text-left">
-                                <DialogStackTitle className="font-semibold text-lg leading-none tracking-tight">
-                                    I'm the first dialog
-                                </DialogStackTitle>
-                                <DialogStackDescription className="text-muted-foreground text-sm">
-                                    With a fancy description
-                                </DialogStackDescription>
+                <div>
+                    <ResponsiveGridLayout
+                        layouts={{lg: layout}}
+                        breakpoints={{lg: 1024, md: 768, sm: 475, xs: 320, xxs: 0}}
+                        cols={{lg: 8, md: 6, sm: 4, xs: 2, xxs: 2}}
+                        rowHeight={131}
+                        onLayoutChange={handleLayoutChange}
+                        isResizable={false}
+                        isDraggable={true}
+                    >
+                        {layout.map(({i}) => (
+                            <div key={i} className="block" style={{background: "#f0f0f0"}}>
+                                {i}
                             </div>
-                            <div className="flex items-center space-x-2 pt-4 justify-end">
-                                <DialogStackNext asChild>
-                                    <Button variant="outline">Next</Button>
-                                </DialogStackNext>
-                            </div>
-                        </DialogStackContent>
-
-                        <DialogStackContent>
-                            <div className="flex flex-col space-y-1.5 text-center sm:text-left">
-                                <DialogStackTitle className="font-semibold text-lg leading-none tracking-tight">
-                                    I'm the second dialog
-                                </DialogStackTitle>
-                                <DialogStackDescription className="text-muted-foreground text-sm">
-                                    With a fancy description
-                                </DialogStackDescription>
-                            </div>
-                            <div className="flex items-center space-x-2 pt-4 justify-between">
-                                <DialogStackPrevious asChild>
-                                    <Button variant="outline">Previous</Button>
-                                </DialogStackPrevious>
-                                <DialogStackNext asChild>
-                                    <Button variant="outline">Next</Button>
-                                </DialogStackNext>
-                            </div>
-                        </DialogStackContent>
-
-                        <DialogStackContent>
-                            <div className="flex flex-col space-y-1.5 text-center sm:text-left">
-                                <DialogStackTitle className="font-semibold text-lg leading-none tracking-tight">
-                                    I'm the final dialog
-                                </DialogStackTitle>
-                                <DialogStackDescription className="text-muted-foreground text-sm">
-                                    With a fancy description
-                                </DialogStackDescription>
-                            </div>
-                            <div className="flex items-center space-x-2 pt-4 justify-start">
-                                <DialogStackPrevious asChild>
-                                    <Button variant="outline">Previous</Button>
-                                </DialogStackPrevious>
-                            </div>
-                        </DialogStackContent>
-                    </DialogStackBody>
-                </DialogStack>
+                        ))}
+                    </ResponsiveGridLayout>
+                </div>
+                <div>
+                    <h3>Current Layout:</h3>
+                    <pre>{JSON.stringify(layout, null, 2)}</pre>
+                </div>
             </div>
         </AppLayout>
     );
