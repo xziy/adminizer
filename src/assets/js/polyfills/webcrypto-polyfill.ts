05916bbe48bbe139548771361ea5b14ef06a5107
VuXfi <x@42.pub>
2025-06-14 20:27:34 +0700

    Refactor CAPTCHA handling in login process; remove webcrypto-polyfill and update admin panel config

--- src/assets/js/polyfills/webcrypto-polyfill.ts
+++
@@ -1,7 +0,0 @@
-import WebCrypto from 'isomorphic-webcrypto';
-
-if (typeof globalThis.crypto === 'undefined' || !globalThis.crypto?.subtle) {
-  (globalThis as any).crypto = WebCrypto as unknown as Crypto;
-}
-
-export default globalThis.crypto as Crypto;
