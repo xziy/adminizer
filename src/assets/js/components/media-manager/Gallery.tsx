9a07c5014a4d93299961d3420d43ed42b24b7f97
markus <markusweblance@gmail.com>
2025-07-08 19:16:45 +0300

    added gallery

---
+++ src/assets/js/components/media-manager/Gallery.tsx
@@ -0,0 +1,66 @@
+import {Input} from "@/components/ui/input.tsx";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
+import {useContext, useEffect, useState} from "react";
+import {MediaManagerContext} from "@/components/media-manager/media-manager.tsx";
+import axios from "axios";
+
+const Gallery = () => {
+    const [mediaType, setMediaType] = useState<string>('all');
+    const { uploadUrl, group } = useContext(MediaManagerContext);
+    const [count, setCount] = useState<number>(5);
+
+    useEffect( () => {
+        const getData = async () => {
+            let data = await axios.get(`${uploadUrl}?count=${count}&skip=0&type=all&group=${group}`)
+            console.log(data.data)
+        }
+        getData();
+    }, []);
+
+    return (
+        <div className="flex justify-between mt-8 gap-4">
+            <Tabs defaultValue="tile-all" className="w-full">
+                <div className="flex gap-4">
+                    <div>
+                        <Input
+                            type="search"
+                            autoFocus={false}
+                            placeholder='Search'
+                            className="w-[200px] p-2 border rounded order-1 xl:order-2"
+                        />
+                    </div>
+                    <TabsList>
+                        <TabsTrigger value="tile-image" onClick={() => setMediaType('image')}>
+                            Images
+                        </TabsTrigger>
+                        <TabsTrigger value="tile-video" onClick={() => setMediaType('video')}>
+                            Video
+                        </TabsTrigger>
+                        <TabsTrigger value="table-text" onClick={() => setMediaType('text')}>
+                            Text
+                        </TabsTrigger>
+                        <TabsTrigger value="table-application"
+                                     onClick={() => setMediaType('application')}>
+                            Application
+                        </TabsTrigger>
+                        <TabsTrigger value="table-all" onClick={() => setMediaType('all')}>
+                            Table
+                        </TabsTrigger>
+                        <TabsTrigger value="tile-all" onClick={() => setMediaType('all')}>
+                            Tile
+                        </TabsTrigger>
+                    </TabsList>
+                </div>
+                <TabsContent value="tile-image">Tile {mediaType}</TabsContent>
+                <TabsContent value="tile-video">Tile {mediaType}</TabsContent>
+                <TabsContent value="tile-text">Tile {mediaType}</TabsContent>
+                <TabsContent value="tile-all">Tile {mediaType}</TabsContent>
+                <TabsContent value="table-text">Table {mediaType}</TabsContent>
+                <TabsContent value="table-application">Table {mediaType}</TabsContent>
+                <TabsContent value="table-all">Table {mediaType}</TabsContent>
+            </Tabs>
+        </div>
+    )
+}
+
+export default Gallery
--- src/lib/media-manager/DefaultMediaManager.ts
+++ src/lib/media-manager/DefaultMediaManager.ts
@@ -1,11 +1,11 @@
 import {
-  AbstractMediaManager,
-  MediaManagerItem,
-  File,
-  MediaManagerWidgetItem,
-  MediaManagerWidgetData,
-  MediaManagerWidgetClientItem,
-  SortCriteria,
+    AbstractMediaManager,
+    MediaManagerItem,
+    File,
+    MediaManagerWidgetItem,
+    MediaManagerWidgetData,
+    MediaManagerWidgetClientItem,
+    SortCriteria,
 } from "./AbstractMediaManager";
 import {populateVariants} from "./helpers/MediaManagerHelper";
 import {ApplicationItem, ImageItem, TextItem, VideoItem} from "./Items";
@@ -13,111 +13,113 @@ import {Adminizer} from "../Adminizer";
 
 
 export class DefaultMediaManager extends AbstractMediaManager {
-  public readonly itemTypes: File<MediaManagerItem>[] = [];
-  public model: string = "mediamanagerap";
-  public modelAssoc: string = "mediamanagerassociationsap";
-  id: string;
-  protected readonly adminizer: Adminizer;
+    public readonly itemTypes: File<MediaManagerItem>[] = [];
+    public model: string = "mediamanagerap";
+    public modelAssoc: string = "mediamanagerassociationsap";
+    id: string;
+    protected readonly adminizer: Adminizer;
 
-  constructor(adminizer: Adminizer, id: string, urlPathPrefix: string, fileStoragePath: string) {
-    super(adminizer);
-    this.id = id;
-    this.urlPathPrefix = urlPathPrefix;
-    this.fileStoragePath = fileStoragePath;
-    this.itemTypes.push(new ImageItem(adminizer, urlPathPrefix, fileStoragePath));
-    this.itemTypes.push(new TextItem(adminizer, urlPathPrefix, fileStoragePath));
-    this.itemTypes.push(new ApplicationItem(adminizer, urlPathPrefix, fileStoragePath));
-    this.itemTypes.push(new VideoItem(adminizer, urlPathPrefix, fileStoragePath));
-    this.adminizer = adminizer;
-  }
-
-  public async getAll(limit: number, skip: number, sort: SortCriteria, group: string): Promise<{
-    data: MediaManagerItem[];
-    next: boolean
-  }> {
-    // TODO refactor CRUD functions for DataAccessor usage
-    let data: MediaManagerItem[] = await this.adminizer.modelHandler.model.get(this.model)["_find"]({
-      where: {parent: null, group: group},
-      limit: limit,
-      skip: skip,
-      sort: sort,
-    }, {populate: [["variants", {sort: sort}], ["meta", {}]]})
-
-    let next = await this.adminizer.modelHandler.model.get(this.model)["_count"]({
-        where: {parent: null, group: group},
-        limit: limit,
-        skip: skip === 0 ? limit : skip + limit,
-        sort: sort,
-      })
-
-    for (let elem of data) {
-      elem.variants = await populateVariants(this.adminizer, elem.variants, this.model)
+    constructor(adminizer: Adminizer, id: string, urlPathPrefix: string, fileStoragePath: string) {
+        super(adminizer);
+        this.id = id;
+        this.urlPathPrefix = urlPathPrefix;
+        this.fileStoragePath = fileStoragePath;
+        this.itemTypes.push(new ImageItem(adminizer, urlPathPrefix, fileStoragePath));
+        this.itemTypes.push(new TextItem(adminizer, urlPathPrefix, fileStoragePath));
+        this.itemTypes.push(new ApplicationItem(adminizer, urlPathPrefix, fileStoragePath));
+        this.itemTypes.push(new VideoItem(adminizer, urlPathPrefix, fileStoragePath));
+        this.adminizer = adminizer;
     }
 
-    return {
-      data: data,
-      next: !!next,
-    };
-  }
+    public async getAll(limit: number, skip: number, sort: SortCriteria, group: string): Promise<{
+        data: MediaManagerItem[];
+        next: boolean
+    }> {
+        // TODO refactor CRUD functions for DataAccessor usage
+        let data: MediaManagerItem[] = await this.adminizer.modelHandler.model.get(this.model)["_find"]({
+            where: {parent: null, group: group},
+            limit: limit,
+            skip: skip,
+            sort: sort,
+        }, {populate: [["variants", {sort: sort}], ["meta", {}]]})
 
-  public async searchAll(s: string, group: string): Promise<MediaManagerItem[]> {
-    // TODO refactor CRUD functions for DataAccessor usage
-    let data: MediaManagerItem[] = await this.adminizer.modelHandler.model.get(this.model)["_find"]({
-      where: {filename: {contains: s}, parent: null, group: group},
-      sort: "createdAt DESC",
-    }, {
-      populate: [["variants", {sort: "createdAt DESC"}], ["meta", {}]],
-      // This limitation is made strictly, if your code solves this please make a PR
-      limit: 1000
-    })
+        console.log(data)
 
+        let next = await this.adminizer.modelHandler.model.get(this.model)["_count"]({
+            where: {parent: null, group: group},
+            limit: limit,
+            skip: skip === 0 ? limit : skip + limit,
+            sort: sort,
+        })
 
-    for (let elem of data) {
-      elem.variants = await populateVariants(this.adminizer, elem.variants, this.model)
+        for (let elem of data) {
+            elem.variants = await populateVariants(this.adminizer, elem.variants, this.model)
+        }
+
+        return {
+            data: data,
+            next: !!next,
+        };
     }
 
-    return data;
-  }
+    public async searchAll(s: string, group: string): Promise<MediaManagerItem[]> {
+        // TODO refactor CRUD functions for DataAccessor usage
+        let data: MediaManagerItem[] = await this.adminizer.modelHandler.model.get(this.model)["_find"]({
+            where: {filename: {contains: s}, parent: null, group: group},
+            sort: "createdAt DESC",
+        }, {
+            populate: [["variants", {sort: "createdAt DESC"}], ["meta", {}]],
+            // This limitation is made strictly, if your code solves this please make a PR
+            limit: 1000
+        })
 
-  public async setRelations(data: MediaManagerWidgetData, model: string, modelId: string, widgetName: string,): Promise<void> {
-    // TODO refactor CRUD functions for DataAccessor usage
-    let modelAssociations = await this.adminizer.modelHandler.model.get(this.modelAssoc)["_find"]({
-      where: {modelId: modelId, model: model, widgetName: widgetName},
-    });
 
-    for (const modelAssociation of modelAssociations) {
-      // TODO refactor CRUD functions for DataAccessor usage
-      await this.adminizer.modelHandler.model.get(this.modelAssoc)["_destroy"](modelAssociation.id)
+        for (let elem of data) {
+            elem.variants = await populateVariants(this.adminizer, elem.variants, this.model)
+        }
+
+        return data;
     }
 
-    for (const [key, widgetItem] of data.list.entries()) {
-      // TODO refactor CRUD functions for DataAccessor usage
-      await this.adminizer.modelHandler.model.get(this.modelAssoc)["_create"]({
-        mediaManagerId: this.id,
-        model: model,
-        modelId: modelId,
-        file: widgetItem.id,
-        widgetName: widgetName,
-        sortOrder: key + 1,
-      });
-    }
-  }
+    public async setRelations(data: MediaManagerWidgetData, model: string, modelId: string, widgetName: string,): Promise<void> {
+        // TODO refactor CRUD functions for DataAccessor usage
+        let modelAssociations = await this.adminizer.modelHandler.model.get(this.modelAssoc)["_find"]({
+            where: {modelId: modelId, model: model, widgetName: widgetName},
+        });
 
-  public async getRelations(items: MediaManagerWidgetItem[],): Promise<MediaManagerWidgetClientItem[]> {
-    let widgetItems: MediaManagerWidgetClientItem[] = [];
-    for (const item of items) {
-      // TODO refactor CRUD functions for DataAccessor usage
-      let file: MediaManagerItem = (await this.adminizer.modelHandler.model.get(this.model)["_find"]({
-        where: {id: item.id}
-      }, {populate: [["variants", {sort: "createdAt DESC"}]]}))[0]
+        for (const modelAssociation of modelAssociations) {
+            // TODO refactor CRUD functions for DataAccessor usage
+            await this.adminizer.modelHandler.model.get(this.modelAssoc)["_destroy"](modelAssociation.id)
+        }
 
-      widgetItems.push({
-        id: file.id,
-        mimeType: file.mimeType,
-        url: file.url,
-        variants: file.variants,
-      });
+        for (const [key, widgetItem] of data.list.entries()) {
+            // TODO refactor CRUD functions for DataAccessor usage
+            await this.adminizer.modelHandler.model.get(this.modelAssoc)["_create"]({
+                mediaManagerId: this.id,
+                model: model,
+                modelId: modelId,
+                file: widgetItem.id,
+                widgetName: widgetName,
+                sortOrder: key + 1,
+            });
+        }
     }
-    return widgetItems;
-  }
+
+    public async getRelations(items: MediaManagerWidgetItem[],): Promise<MediaManagerWidgetClientItem[]> {
+        let widgetItems: MediaManagerWidgetClientItem[] = [];
+        for (const item of items) {
+            // TODO refactor CRUD functions for DataAccessor usage
+            let file: MediaManagerItem = (await this.adminizer.modelHandler.model.get(this.model)["_find"]({
+                where: {id: item.id}
+            }, {populate: [["variants", {sort: "createdAt DESC"}]]}))[0]
+
+            widgetItems.push({
+                id: file.id,
+                mimeType: file.mimeType,
+                url: file.url,
+                variants: file.variants,
+            });
+        }
+        return widgetItems;
+    }
 }
