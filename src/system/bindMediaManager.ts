ef5dbb971228a22afe21b15b5c056ed553262b5a
VuXfi <x@42.pub>
2025-08-28 19:09:13 +0700

    add null check for fileStoragePath in bindMediaManager

--- src/system/bindMediaManager.ts
+++ src/system/bindMediaManager.ts
@@ -20,7 +20,7 @@ export default function bindMediaManager(adminizer: Adminizer) {
 		}
 	})
 	// Bind media manager public folder
-	if(adminizer.config.bind?.public) adminizer.app.use(`/public`, serveStatic(adminizer.config.mediamanager.fileStoragePath));
+	if(adminizer.config.bind?.public && adminizer.config.mediamanager?.fileStoragePath) adminizer.app.use(`/public`, serveStatic(adminizer.config.mediamanager.fileStoragePath));
 
 	// Bind file icons
 	adminizer.app.use(`${adminizer.config.routePrefix}/fileicons`, serveStatic(path.join(import.meta.dirname, '../fileicons')));
