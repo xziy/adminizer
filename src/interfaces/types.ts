f413972a64ea979b8ca6cf2216f4197e45788145
VuXfi <x@42.pub>
2025-09-15 17:13:33 +0700

    feat(migrations): add timestamp property to Migration type

--- src/interfaces/types.ts
+++ src/interfaces/types.ts
@@ -73,6 +73,7 @@ export interface INotificationEvent {
 
 export type Migration = {
   name: string;
+  timestamp: number,
   up: (args: { context: any }) => Promise<unknown> | unknown;
   down: (args: { context: any }) => Promise<unknown> | unknown;
 };
--- src/migrations/umzugExports.ts
+++ src/migrations/umzugExports.ts
@@ -3,6 +3,7 @@ import {up as up0001, down as down0001 } from "./umzug/0001"
 export const umzugExports: Migration[] = [
     {
         name: "0001",
+        timestamp: 1757930548596,
         up: up0001,
         down: down0001
     }   
